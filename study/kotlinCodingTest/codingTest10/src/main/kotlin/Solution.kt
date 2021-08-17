f1aa302af4ea83199af48d8cd5e94db60b0134e8
jaeil <59818827+jaeilssss@users.noreply.github.com>
2021-08-18 00:53:42 +0900

    신규아이디 추천

--- study/kotlinCodingTest/codingTest10/src/main/kotlin/Solution.kt
+++ study/kotlinCodingTest/codingTest10/src/main/kotlin/Solution.kt
@@ -1,46 +1,52 @@
 class Solution {
     fun solution(new_id: String): String {
-        var answer: String = ""
-        answer = new_id
-
+        var answer: String = new_id
         // 1step
         answer = new_id.toLowerCase()
-
         //2step
         var temp = answer
-
-
         for(i in new_id.indices){
             if(!(temp[i].toInt() in 97..122) &&
-                temp[i].equals("-").not() &&
-                temp[i].equals("_").not() &&
-                temp[i].equals(".").not()
-                    ){
-
+                !(temp[i].toInt() in 48..57) &&
+                temp[i].equals('-').not() &&
+                temp[i].equals('_').not() &&
+                temp[i].equals('.').not()
+            ){
                 answer = answer.replace("${temp[i]}","")
             }
         }
-
         //3step
-        temp = answer
         answer = answer.replace("..",".")
-
+        while(true){
+            if(answer.contains("..")){
+                answer = answer.replace("..",".")
+            }else{
+                break
+            }
+        }
         //4step
-        if(answer[0].equals(".")){
-            answer.removeRange(0,1)
+        if(answer[0].equals('.')){
+            answer = answer.removeRange(0,1)
+        }
+        if(answer.length>0){
+            if(answer[answer.length-1] == '.'){
+                answer = answer.removeRange(answer.length-1,answer.length)
+            }
         }
 
+        //5step
         if(answer.length==0){
             answer = "a"
         }
-
+        //6step
         if(answer.length>=16){
-            answer = answer.removeRange(16,answer.length)
-            if(answer[15].equals(".")){
-                answer.removeRange(15,16)
+            answer = answer.removeRange(15,answer.length)
+            if(answer[14].equals('.')){
+                answer = answer.removeRange(14,15)
             }
 
         }
+        //7step
         if(answer.length<=2){
             while(true){
                 if(answer.length==3){
@@ -50,7 +56,6 @@ class Solution {
                 }
             }
         }
-
         return answer
     }
 }
--- study/kotlinCodingTest/codingTest10/src/main/kotlin/main.kt
+++ study/kotlinCodingTest/codingTest10/src/main/kotlin/main.kt
@@ -1,26 +1,21 @@
 fun main(args : Array<String>) {
 
 var str = "a"
-    var new_id = "-#abaa"
+    var new_id = "abcdefghijklmn.p"
     var answer: String = ""
     answer = new_id
     var temp = answer
-    println(str[0].toInt())
-    for(i in new_id.indices){
-        if(temp[i].toInt()==77){
-            println("tlqkf")
-        }
-        if((temp[i].toInt() in 97..122).not() &&
-            (temp[i].toInt()==77) &&
-            (temp[i].toInt()==78) &&
-            (temp[i].toInt()==127)
-        ){
-            if(temp[i].equals(".")){
-                println(temp[i])
-            }
-            println("??")
-            answer = answer.replace("${temp[i]}","")
-        }
+    if(answer[0].equals('.')){
+
+        answer = answer.removeRange(0,1)
+    }
+    println(answer[answer.length-1])
+    if(answer[answer.length-1] == '.'){
+        answer = answer.removeRange(answer.length-1,answer.length)
+    }
+    answer = Solution().solution(new_id)
+    if(answer[14].equals('.')){
+        answer = answer.removeRange(14,15)
     }
     println(answer)
 }//메인 종료
