e8e916843d7eae45f0d97cff980a48aaceeca350
jaeil <59818827+jaeilssss@users.noreply.github.com>
2021-09-03 20:33:26 +0900

    땅따먹기

---
+++ study/kotlinCodingTest/codingTest20/src/Solution.java
@@ -0,0 +1,25 @@
+
+import java.util.stream.Collectors;
+import java.util.*;
+class Solution {
+    int solution(int[][] land) {
+        int answer = 0;
+
+        for(int i = 1 ; i<land.length; i++){
+
+            land[i][0] = Math.max(Math.max(land[i-1][1],land[i-1][2]),land[i-1][3]);
+            land[i][1] = Math.max(Math.max(land[i-1][2],land[i-1][0]),land[i-1][3]);
+            land[i][2] = Math.max(Math.max(land[i-1][1],land[i-1][0]),land[i-1][3]);
+            land[i][3] = Math.max(Math.max(land[i-1][1],land[i-1][0]),land[i-1][2]);
+        }
+        Arrays.sort(land);
+        return land[land.length-1][4];
+    }
+
+    public static void main(String []  args){
+
+
+
+    }
+}
+
