3d0ed1f73172b1f8e9429f27410381a632f2744d
jaeil <59818827+jaeilssss@users.noreply.github.com>
2021-10-01 21:12:17 +0900

    사과게임

--- study/kotlinCodingTest/codingTest36/src/main/kotlin/Solution.kt
+++ study/kotlinCodingTest/codingTest36/src/main/kotlin/Solution.kt
@@ -1,76 +1,16 @@
-import java.util.*
-import kotlin.math.max
-
-class Solution {
-    fun solution(sizes: Array<IntArray>): Int {
-        var answer: Int = 0
-        var max = 0
-        var min = 0
-        if(sizes[0][0]>sizes[0][1]){
-            max = sizes[0][0]
-            min = sizes[0][1]
-        }else{
-            max = sizes[0][1]
-            min = sizes[0][0]
-        }
-
-        for(i in 1 until sizes.size) {
-            var tempMax = 0
-            var tempMin =0
-            if(sizes[i][0]>sizes[i][1]){
-                tempMax = sizes[i][0]
-                tempMin = sizes[i][1]
-            }else{
-                tempMax = sizes[i][1]
-                tempMin = sizes[i][0]
-            }
-            if(max<tempMax){
-                max  = tempMax
-            }
-            if(min<tempMin){
-                min = tempMin
+ class Solution {
+    fun solution(arr1: Array<IntArray>, arr2: Array<IntArray>): Array<IntArray> {
+        val answer = Array(arr1.size) { IntArray(arr2[0].size) }
+        for (i in arr1.indices) {
+            for (j in 0 until arr2[0].size) {
+                for (k in 0 until arr1[0].size) {
+                    answer[i][j] += arr1[i][k] * arr2[k][j]
+                }
             }
         }
-
-        return max*min
+        return answer
     }
 }
 
-fun main(){
-   var num = readLine()?.toInt()
-    var numArray = IntArray(num!!)
 
-    var str = readLine()
-    var array = str?.split(" ")
-    var check = false
-    var answer =1
-    for(i in 0 until num){
-        numArray[i] = array?.get(i)!!.toInt()
-    }
-    Arrays.sort(numArray)
-while(true){
 
-    var temp = answer
-    for(i in numArray.size-1 downTo 0){
-        if(temp>=numArray[i]){
-            if(temp-numArray[i]<0){
-                check = true
-                break
-            }else if(temp-numArray[i]==0){
-                break
-            }else{
-                temp -= numArray[i]
-            }
-        }
-        if(i==0&&temp!=0){
-            check =true
-        }
-    }
-    if(check){
-        println(answer)
-        break
-    }else{
-        answer++
-    }
-}
-}
--- study/kotlinCodingTest/codingTest36/src/main/kotlin/main.kt
+++ study/kotlinCodingTest/codingTest36/src/main/kotlin/main.kt
@@ -1,23 +1,43 @@
 import kotlin.math.min
 
 fun main(){
-    var n = readLine()?.toInt()
-    var num = n!!
     var answer = 0
-    while (true){
-        if(num%5==0){
-            if(num==0){
-                println(answer)
-            }else{
-                println(answer+num/5)
-            }
-            break
-        }else if(num<0){
-            println(-1)
-            break
+    var str = readLine()
+    var count = readLine()?.toInt()
+    var list = ArrayList<Int>()
+    for(i in 0 until count!!){
+        var num = readLine()?.toInt()
+        if (num != null) {
+            list.add(num)
         }
-        num -=3
-        answer++
-
     }
+
+    var chars = str!!.split(" ")
+
+    var bucketWidth = chars[1].toInt()
+    var head = 1
+    var tail = bucketWidth
+
+
+    for(i in list.indices){
+        var apple = list.get(i)
+        if(tail<apple){
+
+            var temp = apple-tail
+
+            answer += temp
+            head +=temp
+            tail+=temp
+
+        }else if(head>apple){
+
+            var temp=head -apple
+
+            answer += temp
+            head -=temp
+            tail-=temp
+        }
+    }
+
+println(answer)
 }
