90e69e7c66d041770773efc5a73be0f070211325
jaeil <59818827+jaeilssss@users.noreply.github.com>
2021-10-01 23:34:32 +0900

    APC는 왜 서브태스크 대회가 되었을까?

--- study/kotlinCodingTest/codingTest36/src/main/kotlin/main.kt
+++ study/kotlinCodingTest/codingTest36/src/main/kotlin/main.kt
@@ -2,42 +2,37 @@ import kotlin.math.min
 
 fun main(){
     var answer = 0
-    var str = readLine()
-    var count = readLine()?.toInt()
-    var list = ArrayList<Int>()
-    for(i in 0 until count!!){
-        var num = readLine()?.toInt()
-        if (num != null) {
-            list.add(num)
+   var str = readLine()
+    var array = str!!.split(" ")
+    var sub1 = ArrayList<Int>()
+    var sub2 = ArrayList<Int>()
+    var map = HashMap<Int,Boolean>()
+    for(i in 0 until array[0].toInt()){
+        var question = readLine()
+        var questionArray = question!!.split(" ")
+        sub1.add(questionArray[0].toInt())
+        sub2.add(questionArray[1].toInt())
+    }
+    sub1.sortDescending()
+    sub2.sortDescending()
+    for(i in sub2.indices ){
+        if(map.size==array[2].toInt()){
+            break
+        }
+        if(map.size<=array[2].toInt() && sub2[i].toInt()<=array[1].toInt()){
+            answer +=140
+            map.put(i,true)
         }
     }
-
-    var chars = str!!.split(" ")
-
-    var bucketWidth = chars[1].toInt()
-    var head = 1
-    var tail = bucketWidth
-
-
-    for(i in list.indices){
-        var apple = list.get(i)
-        if(tail<apple){
-
-            var temp = apple-tail
-
-            answer += temp
-            head +=temp
-            tail+=temp
-
-        }else if(head>apple){
-
-            var temp=head -apple
-
-            answer += temp
-            head -=temp
-            tail-=temp
+    for(i in 0 until array[0].toInt()){
+        if(map.size==array[2].toInt()){
+            break
         }
-    }
+        if(sub1[i].toInt()<=array[1].toInt() && map.containsKey(i).not()){
+            answer +=100
+            map.put(i,true)
+        }
 
+    }
 println(answer)
 }
