4c38e23109c412a150af875d894b9d9c0a872411
jaeil <59818827+jaeilssss@users.noreply.github.com>
2021-10-03 12:21:04 +0900

    A->b

--- study/kotlinCodingTest/codingTest36/src/main/kotlin/main.kt
+++ study/kotlinCodingTest/codingTest36/src/main/kotlin/main.kt
@@ -1,38 +1,37 @@
+import sun.misc.Queue
+
+import kotlin.collections.HashMap
+import java.util.*
 import kotlin.math.min
 
-fun main(){
-    var answer = 0
-   var str = readLine()
-    var array = str!!.split(" ")
-    var sub1 = ArrayList<Int>()
-    var sub2 = ArrayList<Int>()
-    var map = HashMap<Int,Boolean>()
-    for(i in 0 until array[0].toInt()){
-        var question = readLine()
-        var questionArray = question!!.split(" ")
-        sub1.add(questionArray[0].toInt())
-        sub2.add(questionArray[1].toInt())
-    }
-    sub1.sortDescending()
-    sub2.sortDescending()
-    for(i in sub2.indices ){
-        if(map.size==array[2].toInt()){
-            break
-        }
-        if(map.size<=array[2].toInt() && sub2[i].toInt()<=array[1].toInt()){
-            answer +=140
-            map.put(i,true)
-        }
-    }
-    for(i in 0 until array[0].toInt()){
-        if(map.size==array[2].toInt()){
-            break
-        }
-        if(sub1[i].toInt()<=array[1].toInt() && map.containsKey(i).not()){
-            answer +=100
-            map.put(i,true)
-        }
+var minimum : Long = 1000000000L
 
+fun main() = with(System.`in`.bufferedReader()){
+    val (a,b) = readLine().split(" ").map { it.toLong() }
+
+    find(a,b,0)
+
+    if(minimum == 1000000000L){
+        println("-1")
     }
-println(answer)
+    else println(minimum+1)
+
 }
+
+fun find(a : Long, b : Long, count : Long){
+
+    if(a==b) {
+        minimum = min(minimum,count)
+        return
+    }
+
+    else if(a>b){
+        return
+    }
+
+    find(a*2,b,count+1)
+    find((a.toString()+"1").toLong(),b,count+1)
+}
+
+
+
