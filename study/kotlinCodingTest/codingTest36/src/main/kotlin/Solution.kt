42b928ac6d4b0d560f26ab67c704812f95091b5a
jaeil <59818827+jaeilssss@users.noreply.github.com>
2021-10-10 15:01:58 +0900

    타겟넘버

--- study/kotlinCodingTest/codingTest36/src/main/kotlin/Solution.kt
+++ study/kotlinCodingTest/codingTest36/src/main/kotlin/Solution.kt
@@ -1,16 +1,38 @@
- class Solution {
-    fun solution(arr1: Array<IntArray>, arr2: Array<IntArray>): Array<IntArray> {
-        val answer = Array(arr1.size) { IntArray(arr2[0].size) }
-        for (i in arr1.indices) {
-            for (j in 0 until arr2[0].size) {
-                for (k in 0 until arr1[0].size) {
-                    answer[i][j] += arr1[i][k] * arr2[k][j]
-                }
-            }
-        }
+class Solution {
+    var answer = 0
+    var size =0
+    var wantedNumber  =0
+    lateinit var numbers : IntArray
+    fun solution(numbers: IntArray, target: Int): Int {
+        wantedNumber = target
+        size = numbers.size
+                this.numbers = numbers
+        search(numbers[0],0,1)
+        search(numbers[0]*-1,0,1)
         return answer
     }
+    fun search(num : Int,sum : Int ,index :Int){
+        var temp = sum+num
+
+        if(index==size){
+
+            if(temp==wantedNumber){
+
+               answer++
+            }
+            return
+        }
+
+        search(numbers[index],temp,index+1)
+        search(numbers[index]*-1,temp,index+1)
+
+    }
 }
+fun main(){
+    println(Solution().solution(intArrayOf(1,1,1,1,1),3))
+}
+
+
 
 
 
--- study/kotlinCodingTest/codingTest36/src/main/kotlin/main.kt
+++ study/kotlinCodingTest/codingTest36/src/main/kotlin/main.kt
@@ -1,37 +1,29 @@
-import sun.misc.Queue
-
-import kotlin.collections.HashMap
 import java.util.*
-import kotlin.math.min
+import kotlin.collections.ArrayList
 
-var minimum : Long = 1000000000L
+fun main(){
+    var array = IntArray(46)
 
-fun main() = with(System.`in`.bufferedReader()){
-    val (a,b) = readLine().split(" ").map { it.toLong() }
-
-    find(a,b,0)
-
-    if(minimum == 1000000000L){
-        println("-1")
+    var n  = readLine()!!.toInt()
+    var list = ArrayList<Int>()
+    var fibo = ArrayList<Int>()
+    array[0]=1
+    array[1]=1
+    for(i in 0 until n){
+        var num = readLine()!!.toInt()
+        list.add(num)
     }
-    else println(minimum+1)
+
+    for(i in 2 until 46){
+        array[i] = array[i-2]+array[i-1]
+    }
+
+    for(i in 0 until n){
+        
+    }
+
+
 
 }
-
-fun find(a : Long, b : Long, count : Long){
-
-    if(a==b) {
-        minimum = min(minimum,count)
-        return
-    }
-
-    else if(a>b){
-        return
-    }
-
-    find(a*2,b,count+1)
-    find((a.toString()+"1").toLong(),b,count+1)
-}
-
 
 
