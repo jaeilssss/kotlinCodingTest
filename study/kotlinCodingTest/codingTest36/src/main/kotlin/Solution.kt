a4fa6947d8c9ed9ffcafe368832b680c84b3f9f1
jaeil <59818827+jaeilssss@users.noreply.github.com>
2021-10-10 23:35:09 +0900

    bfs,dfs

--- study/kotlinCodingTest/codingTest36/src/main/kotlin/Solution.kt
+++ study/kotlinCodingTest/codingTest36/src/main/kotlin/Solution.kt
@@ -1,38 +1,19 @@
 class Solution {
-    var answer = 0
-    var size =0
-    var wantedNumber  =0
-    lateinit var numbers : IntArray
-    fun solution(numbers: IntArray, target: Int): Int {
-        wantedNumber = target
-        size = numbers.size
-                this.numbers = numbers
-        search(numbers[0],0,1)
-        search(numbers[0]*-1,0,1)
+    fun solution(n: Int, wires: Array<IntArray>): Int {
+        var answer: Int = -1
+        var dfsGraph: Array<IntArray?>
+
+
+        var arr1 = Array<IntArray>(5){IntArray(4)}
+
         return answer
     }
-    fun search(num : Int,sum : Int ,index :Int){
-        var temp = sum+num
-
-        if(index==size){
-
-            if(temp==wantedNumber){
-
-               answer++
-            }
-            return
-        }
-
-        search(numbers[index],temp,index+1)
-        search(numbers[index]*-1,temp,index+1)
-
-    }
 }
+
 fun main(){
-    println(Solution().solution(intArrayOf(1,1,1,1,1),3))
+    var arr1 = Array<IntArray>(5){IntArray(4)}
+    arr1[0][0]=1
+    println(arr1[0][0])
 }
-
-
-
 
 
--- study/kotlinCodingTest/codingTest36/src/main/kotlin/main.kt
+++ study/kotlinCodingTest/codingTest36/src/main/kotlin/main.kt
@@ -1,28 +1,55 @@
 import java.util.*
 import kotlin.collections.ArrayList
 
+var visited = booleanArrayOf()
+var array : Array<IntArray> = arrayOf()
+var strArray :List<String> = listOf()
 fun main(){
-    var array = IntArray(46)
 
-    var n  = readLine()!!.toInt()
-    var list = ArrayList<Int>()
-    var fibo = ArrayList<Int>()
-    array[0]=1
-    array[1]=1
-    for(i in 0 until n){
-        var num = readLine()!!.toInt()
-        list.add(num)
-    }
+    var str = readLine()
 
-    for(i in 2 until 46){
-        array[i] = array[i-2]+array[i-1]
-    }
+     strArray = str!!.split(" ")
+    array = Array(strArray[0].toInt()){ IntArray(strArray[0].toInt()) }
+    visited = BooleanArray(strArray[0].toInt())
+    for(i in 0 until strArray[1].toInt()){
+        var input = readLine()
+        var temp = input!!.split(" ")
 
-    for(i in 0 until n){
-        
+      array[temp[0].toInt()-1][temp[1].toInt()-1]= 1
+        array[temp[1].toInt()-1][temp[0].toInt()-1]=1
+
     }
 
 
+}
+fun dfs(v : Int){
+
+    visited[v] =true
+
+    for(i in 0 until strArray[0].toInt()){
+        if(array[v][i]!=0 && !visited[i]){
+            dfs(i)
+        }
+    }
+
+}
+
+fun bfs(v : Int){
+    val list : LinkedList<Int> = LinkedList()
+    list.add(v)
+    visited[v]=true
+
+    while (list.isNotEmpty()){
+        val now = list.poll()
+
+        for(i in 0 until strArray[0].toInt()){
+            if(!visited[i] && array[now][i]!=0){
+                list.add(i)
+                visited[i]=true
+
+            }
+        }
+    }
 
 }
 
