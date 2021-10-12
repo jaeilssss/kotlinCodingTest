c2c6a11d96d04873f39f2f66f86bd48203fe1007
jaeil <59818827+jaeilssss@users.noreply.github.com>
2021-10-12 11:47:11 +0900

    미로탐색

--- study/kotlinCodingTest/codingTest36/src/main/kotlin/main.kt
+++ study/kotlinCodingTest/codingTest36/src/main/kotlin/main.kt
@@ -1,59 +1,53 @@
 import java.util.*
 
-var visited = booleanArrayOf()
-var array : Array<IntArray> = arrayOf()
-var strArray :List<String> = listOf()
-fun main(){
+var visited : Array<BooleanArray> = arrayOf()
 
-    var str = readLine()
+var graphs : Array<IntArray> = arrayOf()
+var dx = intArrayOf(-1,0,1,0)
+var dy = intArrayOf(0,-1,0,1)
 
-    strArray = str!!.split(" ")
-    array = Array(strArray[0].toInt()){ IntArray(strArray[0].toInt()) }
-    visited = BooleanArray(strArray[0].toInt())
-    for(i in 0 until strArray[1].toInt()){
-        var input = readLine()
-        var temp = input!!.split(" ")
+class Pointer( var x : Int = 0,
+               var y : Int = 0)
 
-        array[temp[0].toInt()-1][temp[1].toInt()-1]= 1
-        array[temp[1].toInt()-1][temp[0].toInt()-1]=1
+fun main() {
 
-    }
-    visited.fill(false)
-    dfs(strArray[2].toInt()-1)
-    println("")
-    visited.fill(false)
-    bfs(strArray[2].toInt()-1)
+    var temp = readLine()
 
-}
-fun dfs(v : Int){
-    print("${v+1} ")
-    visited[v] =true
+    var array = temp!!.split(" ").map { it.toInt() }
 
-    for(i in 0 until strArray[0].toInt()){
-        if(array[v][i]!=0 && !visited[i]){
-            dfs(i)
+    visited = Array(array[0]+2){ BooleanArray(array[1]+2) }
+    graphs = Array(array[0]+2){ IntArray(array[1]+2) }
+
+
+    for(i in 1 .. array[0]){
+        var temp  = readLine()!!.toCharArray()
+
+        for(j in 1 .. array[1]){
+            graphs[i][j] = Integer.parseInt(temp.get(j-1).toString())
         }
     }
 
+    bfs(1,1)
+
+    println(graphs[array[0]][array[1]])
 }
 
-fun bfs(v : Int){
-    val list : LinkedList<Int> = LinkedList()
-    list.add(v)
-    visited[v]=true
-    print("${v+1} ")
-    while (list.isNotEmpty()){
-        val now = list.poll()
+fun bfs(x : Int, y : Int){
+    var queue = LinkedList<Pointer>()
+    queue.add(Pointer(x,y))
+    visited[x][y]=true
+    while (queue.isNotEmpty()){
+        var pointer = queue.poll()
+        for(i in 0 until 4){
 
-        for(i in 0 until strArray[0].toInt()){
-            if(!visited[i] && array[now][i]!=0){
-                list.add(i)
-                visited[i]=true
-                print("${i+1} ")
+            var tempX = pointer.x+dx[i]
+            var tempY = pointer.y+dy[i]
+
+            if(graphs[tempX][tempY]!=0 && !visited[tempX][tempY]){
+                visited[tempX][tempY] =true
+                graphs[tempX][tempY] = graphs[pointer.x][pointer.y]+1
+                queue.add(Pointer(tempX,tempY))
             }
         }
     }
-
 }
-
-
---
+++ study/kotlinCodingTest/codingTest36/src/main/kotlin/temp.kt
@@ -0,0 +1,14 @@
+
+
+
+fun main(){
+
+    var temp = readLine()
+
+    var array = temp!!.split(" ").map { it.toInt() }
+
+
+
+
+
+}
