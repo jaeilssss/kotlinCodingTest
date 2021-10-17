4601b107379d89386ca73857a6b17f8c2c9efb21
jaeil <59818827+jaeilssss@users.noreply.github.com>
2021-10-17 16:14:09 +0900

    유기농배추

--- study/kotlinCodingTest/codingTest36/src/main/kotlin/main.kt
+++ study/kotlinCodingTest/codingTest36/src/main/kotlin/main.kt
@@ -9,43 +9,38 @@ var dy = intArrayOf(0,-1,0,1)
 var answer = 0
 var count = 0
 fun main(){
-  var size = readLine()!!.toInt()
+  var num = readLine()!!.toInt()
     var list = ArrayList<Int>()
-    graphs = Array(size+2){ IntArray(size+2) }
-    visited = Array(size+2){ BooleanArray(size+2) }
-    for(i in 1 until size+1){
-        var input = readLine()!!.toCharArray()
-
-        for(j in 1 until size+1){
-
-            graphs[i][j]=Integer.parseInt(input[j-1].toString())
+    for(i in 0 until num){
+        answer=0
+        var str = readLine()!!.split(" ")
+        graphs = Array(str[0].toInt()+2){ IntArray(str[1].toInt()+2) }
+        visited = Array(str[0].toInt()+2){ BooleanArray(str[1].toInt()+2) }
+        for(j in 0 until str[2].toInt()){
+            var input = readLine()!!.split(" ")
+            graphs[input[0].toInt()+1][input[1].toInt()+1]=1
         }
-    }
 
-    for(i in 1 until size+1){
-        for(j in 1 until size+1){
-            if(!visited[i][j] && graphs[i][j]==1){
-                answer=0
-                dfs(i,j)
-                visited[i][j]=true
-                count++
-                list.add(answer)
+        for(a in 1 until str[0].toInt()+1){
+            for(m in 1 until str[1].toInt()+1){
+                if(!visited[a][m] && graphs[a][m]==1){
+                    dfs(a,m)
+                    answer++
+                }
             }
         }
+        list.add(answer)
     }
+for(i in list.indices){
+    println(list.get(i))
+}
 
-    list.sort()
-    println(count)
-    for(i in list.indices){
-
-        println(list[i])
-    }
 
 
 }
 fun dfs(x : Int , y : Int){
 
-    answer++
+
     visited[x][y] = true
     for(i in 0..3){
 
