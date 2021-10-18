334f16b2a0afd5884684f77ba45f8eedd279aaf4
jaeil <59818827+jaeilssss@users.noreply.github.com>
2021-10-18 18:02:19 +0900

    죽음의게임

--- study/kotlinCodingTest/codingTest36/src/main/kotlin/main.kt
+++ study/kotlinCodingTest/codingTest36/src/main/kotlin/main.kt
@@ -2,39 +2,48 @@
 import java.lang.StringBuilder
 import java.util.*
 import kotlin.collections.ArrayList
-
+var graphs : Array<ArrayList<Int>> = arrayOf()
+var answer = 0
+var count = 0
+var friendNum =0
+var check  = false
 fun main(){
-var array = readLine()!!.split(" ").map { it.toInt() }
-    var newArray : IntArray = array.toIntArray()
-    array = array.sorted()
-var left = 0
-    var right = 1
-while (true){
-    if(right==5){
-        left = 0
-        right=1
+var str = readLine()!!.split(" ").map { it.toInt() }
+    graphs = Array(str[0]){ ArrayList() }
+    friendNum = str[1]
+    for(i in 0 until str[0]){
+        var temp = readLine()!!.toInt()
+        graphs[i].add(temp)
     }
-    if(newArray[left]>newArray[right]){
-        var check = true
-        var temp = newArray[left]
-        newArray[left] = newArray[right]
-        newArray[right] = temp
-        for(i in 0..4){
-            if(newArray[i]!=array[i]){
-                check = false
-            }
-            print("${newArray[i]} ")
-        }
-        println()
-        if(check){
-            break
-        }
-    }
-    left++
-    right++
+    dfs(0)
 
 }
 
+fun dfs(v : Int){
+    count++
+
+    if(v==friendNum && !check){
+
+        println(answer)
+        check= true
+        return
+    }
+    if(count==graphs.size+1){
+        if(!check){
+            println(-1)
+        }
+        return
+    }
+    for( i in 0 until graphs[v].size){
+        answer++
+        var data = graphs[v].get(i)
+        if(check){
+            break
+        }else{
+            dfs(data)
+        }
+
+    }
 }
 
 
