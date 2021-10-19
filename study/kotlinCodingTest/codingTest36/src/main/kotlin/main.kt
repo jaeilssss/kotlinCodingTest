b29c5b9f0c22c3c38b91b705e7033dd4c21ba2e9
jaeil <59818827+jaeilssss@users.noreply.github.com>
2021-10-19 17:49:19 +0900

    구름

--- study/kotlinCodingTest/codingTest36/src/main/kotlin/main.kt
+++ study/kotlinCodingTest/codingTest36/src/main/kotlin/main.kt
@@ -1,49 +1,36 @@
 
-import java.lang.StringBuilder
-import java.util.*
-import kotlin.collections.ArrayList
-var graphs : Array<ArrayList<Int>> = arrayOf()
-var answer = 0
-var count = 0
-var friendNum =0
-var check  = false
 fun main(){
-var str = readLine()!!.split(" ").map { it.toInt() }
-    graphs = Array(str[0]){ ArrayList() }
-    friendNum = str[1]
+
+    var str = readLine()!!.split(" ").map { it.toInt() }
+
+    var array :Array<IntArray> = Array(str[0]){ IntArray(str[1]) }
     for(i in 0 until str[0]){
-        var temp = readLine()!!.toInt()
-        graphs[i].add(temp)
+        var temp = readLine()!!.toCharArray()
+        var count = -1
+        for(j in 0 until str[1]){
+            if(temp[j].equals('.')){
+                array[i][j] = count
+                if(count!=-1){
+                    count++
+                }
+            }else if(temp[j].equals('c')){
+                count = 1
+                array[i][j]=0
+            }
+        }
     }
-    dfs(0)
+
+    for(i in 0 until str[0]){
+        for(j in 0 until str[1]){
+            print("${array[i][j]} ")
+        }
+        println()
+    }
 
 }
 
-fun dfs(v : Int){
-    count++
 
-    if(v==friendNum && !check){
 
-        println(answer)
-        check= true
-        return
-    }
-    if(count==graphs.size+1){
-        if(!check){
-            println(-1)
-        }
-        return
-    }
-    for( i in 0 until graphs[v].size){
-        answer++
-        var data = graphs[v].get(i)
-        if(check){
-            break
-        }else{
-            dfs(data)
-        }
 
-    }
-}
 
 
--- study/kotlinCodingTest/codingTest36/src/main/kotlin/test.kt
+++ study/kotlinCodingTest/codingTest36/src/main/kotlin/test.kt
@@ -1,52 +1,23 @@
 import kotlin.math.min
+var array :Array<CharArray> = arrayOf()
+fun main(){
+    var size = readLine()!!.toInt()
+    var max = 0
+    array = Array(size){ CharArray(size) }
 
+    for(i in 0 until size){
+        var str = readLine()!!.toCharArray()
 
-
-
-    fun main(args: Array<String>) = with(System.`in`.bufferedReader()) {
-        println("dd")
-        val n = readLine().toInt()
-        val switches = BooleanArray(n) { false }
-        readLine()
-            .split(" ")
-            .forEachIndexed { index, s ->
-                switches[index] = s != "0"
-            }
-        val t = readLine().toInt()
-
-        for (i in 0 until t) {
-            var (gender, idx) = readLine().split(" ").map { it.toInt() }
-            idx--
-
-            when (gender) {
-                1 -> {
-                    for (j in idx until n step (idx + 1)) {
-                        switches[j] = !switches[j]
-                    }
-                }
-                else -> {
-                    val min = min(n - (idx + 1), idx)
-
-                    for (j in 0..min) {
-                        if (j != 0) {
-                            if (switches[idx - j] == switches[idx + j]) {
-                                switches[idx + j] = !switches[idx + j]
-                                switches[idx - j] = !switches[idx - j]
-                            } else {
-                                break
-                            }
-                        } else {
-                            switches[idx] = !switches[idx]
-                        }
-                    }
-                }
-            }
+        for(j in 0 until size){
+            array[i][j]=str[j]
         }
-
-        println(
-            switches
-                .map { if (it) 1 else 0 }
-                .chunked(20)
-                .joinToString("\n") { it.joinToString(" ") }
-        )
     }
+
+    for(i in 0 until size){
+
+        for(j in 0 until size){
+
+        }
+    }
+
+}
