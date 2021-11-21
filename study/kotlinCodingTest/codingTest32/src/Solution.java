ed523528660e333345a1fd71779ed477d1c8c396
jaeil <59818827+jaeilssss@users.noreply.github.com>
2021-11-21 20:29:49 +0900

    H-index

--- study/kotlinCodingTest/codingTest32/src/Solution.java
+++ study/kotlinCodingTest/codingTest32/src/Solution.java
@@ -1,46 +1,25 @@
 
+import java.util.Arrays;
+
 class Solution {
-    public String solution(int n, int t, int m, int p) {
-        StringBuffer answer = new StringBuffer();
-        int num=1;
-        int count=0;
-        int index =1;
-        StringBuffer str = new StringBuffer();
-        str.append("0");
-        while (str.toString().length()<t*m){
-            String temp = conversion(num++,n);
-            str.append(temp);
-        }
-        String result= str.toString();
 
-        while(count<t){
-           index= (m*count)+p-1;
-            answer.append(result.charAt(index));
-            count++;
-        }
-        return answer.toString();
-    }
+    public int solution(int[] citations) {
+        int answer = 0;
 
-    public  String conversion(int number, int N){
-        StringBuilder sb = new StringBuilder();
-        int current = number;
+        Arrays.sort(citations);
 
-
-        while(current > 0){
-
-            if(current % N < 10){
-                sb.append(current % N);
-
-            } else {
-                sb.append((char)(current % N - 10 + 'A'));
+        for(int i = 0; i < citations.length; i++) {
+            if(citations[i] >= citations.length - i) {
+                answer = citations.length - i;
+                break;
             }
-            current /= N;
         }
-            return sb.reverse().toString();
 
+        return answer;
     }
 
-    public static void main(String [] args){
-        System.out.println(new Solution().solution(2,4,2,1));
-    }
+public static void main(String [] args){
+        System.out.println(new Solution().solution(new int[]{25,8,5,3,3}));
+}
+
 }
