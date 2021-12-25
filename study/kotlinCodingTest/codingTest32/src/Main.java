4650eb7100e55c868ba97f9f23b1b1d54720e7d5
jaeil <59818827+jaeilssss@users.noreply.github.com>
2021-12-25 17:46:37 +0900

    결혼식

--- study/kotlinCodingTest/codingTest32/src/Main.java
+++ study/kotlinCodingTest/codingTest32/src/Main.java
@@ -1,38 +1,50 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
 
+   static boolean[] visited ;
+    static int n;
+    static int m;
+   static int answer = 0;
+   static HashMap<Integer,Integer> map;
+    static int [][] graph;
     public static void main(String [] args){
-
-        int answer = 0;
 
         Scanner scanner = new Scanner(System.in);
 
-        int n = scanner.nextInt();
-        int length = scanner.nextInt();
-        int count = n;
-        char [] array = scanner.next().toCharArray();
+        n = scanner.nextInt();
+        m = scanner.nextInt();
+        map = new HashMap<>();
+        graph = new int[n][n];
+        visited = new boolean[n];
 
-        for(int i = 1; i<length-1; i++){
+        for(int i = 0 ; i<m;i++){
+            int nod1 = scanner.nextInt();
+            int nod2 = scanner.nextInt();
 
-            if(array[i-1]=='I' && array[i]=='O' && array[i+1]=='I'){
-                count--;
-                if(count==0){
-                    answer++;
-                    count++;
-                }
-                i++;
-            }else{
-                count=n;
-            }
+            graph[nod1-1][nod2-1]=1;
+            graph[nod2-1][nod1-1]=1;
 
         }
+        dfs(0,0);
+
+        System.out.println(map.keySet().size());
+
+    }
 
 
-        System.out.println(answer);
-
+    public static void dfs(int depth , int nod){
+        if(depth>=2) { return; }
+        visited[nod] =true;
+        for(int  i = 1; i<n;i++){
+            if(graph[nod][i]==1){
+                    dfs(depth+1,i);
+                    map.put(i,i);
+            }
+        }
     }
 }
