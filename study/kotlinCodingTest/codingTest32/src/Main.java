513e2ddc84107b0f2423f4ae4058e6401fb36576
jaeil <59818827+jaeilssss@users.noreply.github.com>
2022-01-08 15:26:57 +0900

    김식당

--- study/kotlinCodingTest/codingTest32/src/Main.java
+++ study/kotlinCodingTest/codingTest32/src/Main.java
@@ -4,62 +4,74 @@ import sun.security.util.Password;
 import javax.swing.text.Style;
 
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.awt.*;
+import java.util.*;
 import java.io.*;
 public class Main {
     static String S;
     static String T;
+    public static class PostIt {
+        int tableNum;
+        int time;
+
+        public PostIt(int tableNum, int time) {
+            this.tableNum = tableNum;
+            this.time = time;
+        }
+
+
+    }
     public static void main(String [] args) throws IOException {
 
         Scanner scanner = new Scanner(System.in);
-        String str = scanner.nextLine();
-        ArrayList<String> list = new ArrayList<>();
-        String temp="";
-        boolean isTag= false;
-        for(int i = 0 ; i<str.length();i++){
 
-            if(str.substring(i, i + 1).equals("<")){
-                if(temp.length()!=0){
-                    StringBuilder sb = new StringBuilder(temp);
-                    sb.reverse();
-                    list.add(sb.toString());
-                    temp =str.substring(i,i+1);
-                    isTag = true;
-                }else{
-                    temp +=str.substring(i,i+1);
-                    isTag = true;
+
+        String first  = scanner.nextLine();
+        int n = Integer.parseInt(first.split(" ")[0]);
+        ArrayList<String> order = new ArrayList<>();
+        ArrayList<PostIt> list=  new ArrayList<>();
+        for(int i = 0; i<n;i++){
+            String str = scanner.nextLine();
+            order.add(str);
+
+        }
+
+        for(int i = 0 ; i<order.size() ; i++){
+            String [] input = order.get(i).split(" ");
+
+            if(input[0].equals("order")){
+                list.add(new PostIt(Integer.parseInt(input[1]),Integer.parseInt(input[2])));
+            }else if(input[0].equals("sort")){
+                Collections.sort(list, new Comparator<PostIt>() {
+                    @Override
+                    public int compare(PostIt o1, PostIt o2) {
+                        if(o1.time==o2.time){
+                            return o1.tableNum-o2.tableNum;
+                        }else{
+                            return o1.time-o2.time;
+                        }
+                    }
+                });
+
+            }else if(input[0].equals("complete")){
+                for(int  j=0; j<list.size();j++){
+                    if(list.get(j).tableNum==Integer.parseInt(input[1])){
+                        list.remove(j);
+                    }
                 }
-            } else if(str.substring(i, i + 1).equals(">")){
-                temp +=str.substring(i,i+1);
-                list.add(temp);
-                temp="";
-                isTag = false;
-            }else if(str.substring(i, i + 1).equals(" ")){
-                if(temp.length()!=0 && !isTag){
-                    StringBuilder sb = new StringBuilder(temp);
-                    sb.reverse();
-                    list.add(sb.toString());
-                    list.add(" ");
-                    temp="";
-                }else{
-                    temp +=str.substring(i,i+1);
+            }
+
+            if(list.size()!=0){
+                for(int j = 0 ;j<list.size();j++){
+                    System.out.print(list.get(j).tableNum+" ");
                 }
+                System.out.println();
             }else{
-                temp +=str.substring(i,i+1);
+                System.out.println("sleep");
             }
         }
-        if(temp.length()!=0) {
-            StringBuilder sb = new StringBuilder(temp);
-            sb.reverse();
-            list.add(sb.toString());
-        }
-        for(int i = 0; i<list.size();i++){
-            System.out.print(list.get(i));
-        }
-    }
 
+
+    }
 
 }
