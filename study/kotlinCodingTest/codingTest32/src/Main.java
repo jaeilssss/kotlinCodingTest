e9d9a2efa6a1fbb8024e1953210245c96e3d8f88
jaeil <59818827+jaeilssss@users.noreply.github.com>
2022-01-13 16:29:15 +0900

    등수매기기

--- study/kotlinCodingTest/codingTest32/src/Main.java
+++ study/kotlinCodingTest/codingTest32/src/Main.java
@@ -10,38 +10,28 @@ import java.io.*;
 import java.util.*;
 public class Main {
 
-
+   static int n;
+    static int [] array;
+    static long sum=0;
     public static void main(String [] args){
-        Scanner scanner = new Scanner(System.in);
 
-        int n = scanner.nextInt();
-        int m = scanner.nextInt();
-        int area=1;
-        int  [][] arr = new int[n][m];
+        Scanner scanner=  new Scanner(System.in);
 
+        n = scanner.nextInt();
 
-        for(int i = 0 ;i<n;i++){
-            String str = scanner.next();
-            for(int j= 0;j<m;j++){
-                arr[i][j]=Integer.parseInt(str.substring(j,j+1));
-            }
+        array= new int[n];
+
+        for(int i = 0; i<n;i++){
+            int num = scanner.nextInt();
+            array[i] = num;
         }
-
-
+        Arrays.sort(array);
         for(int i=0;i<n;i++){
-
-            for(int j =0; j<m-1; j++){
-                for(int k=1; k<m;k++){
-                    if(i+k<n && j+k<m &&
-                    arr[i][j]==arr[i][j+k]
-                    &&arr[i][j+k]==arr[i+k][j]
-                    &&arr[i+k][j+k]==arr[i][j]){
-                        area = Math.max(area,(k+1)*(k+1));
-                    }
-                }
+            if(array[i]!=i+1){
+                sum+=Math.abs( array[i]-(i+1));
             }
         }
 
-        System.out.print(area);
+        System.out.println(sum);
     }
 }
