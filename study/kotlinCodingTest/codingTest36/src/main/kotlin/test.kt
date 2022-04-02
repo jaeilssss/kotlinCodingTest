183c8e479b894a2fae3f714adb58a57719eb1a27
jaeil <59818827+jaeilssss@users.noreply.github.com>
2022-04-02 17:04:33 +0900

    통계학

--- study/kotlinCodingTest/codingTest36/src/main/kotlin/test.kt
+++ study/kotlinCodingTest/codingTest36/src/main/kotlin/test.kt
@@ -1,23 +1,47 @@
-import kotlin.math.min
-var array :Array<CharArray> = arrayOf()
-fun main(){
-    var size = readLine()!!.toInt()
-    var max = 0
-    array = Array(size){ CharArray(size) }
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.lang.Math.round
 
-    for(i in 0 until size){
-        var str = readLine()!!.toCharArray()
-
-        for(j in 0 until size){
-            array[i][j]=str[j]
+val br = BufferedReader(InputStreamReader(System.`in`))
+fun main()=with(br){
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    val num = readLine()!!.toInt()
+    var list = mutableListOf<Int>()
+    var key = HashMap<Int,Int>()
+    for(i in 0 until num){
+        val go = readLine()!!.toInt()
+        list.add(go)
+        if(!key.containsKey(go)){
+            key[go]=0
+        }
+        key[go]=key[go]!!+1
+    }
+    //println(key)
+    var list3 = key.toList().sortedWith(Comparator { d1, d2 ->
+        d1.first - d2.first
+    })
+    var list2 = list3.sortedWith(Comparator { d1, d2 ->
+        d2.second - d1.second
+    })
+    //println(list2)
+    list.sort()
+    //println(list)
+    // println(key)
+    bw.write("${round(list.average())}\n")
+    bw.write("${list[num/2]}\n")
+    if(num==1) {
+        bw.write("${list2[0].first}\n")
+    }
+    else {
+        if (list2[0].second > list2[1].second) {
+            bw.write("${list2[0].first}\n")
+        } else {
+            bw.write("${list2[1].first}\n")
         }
     }
-
-    for(i in 0 until size){
-
-        for(j in 0 until size){
-
-        }
-    }
-
+    bw.write("${list.maxByOrNull { it }!! - list.minByOrNull { it }!!}\n")
+    bw.flush()
+    bw.close()
 }
