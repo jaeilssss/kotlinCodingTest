47c55f24e7329df01bc0baf9ffb131b6e4ddae9a
jaeil <59818827+jaeilssss@users.noreply.github.com>
2022-04-21 00:06:43 +0900

    백준 문자열

--- study/kotlinCodingTest/codingTest36/src/main/kotlin/main.kt
+++ study/kotlinCodingTest/codingTest36/src/main/kotlin/main.kt
@@ -2,53 +2,23 @@ import kotlin.math.abs
 val bre = System.`in`.bufferedReader()
 fun main() = with(System.out.bufferedWriter()){
 
-    var (n,m,b) = bre.readLine().split(" ").map { it.toInt() }
-    var block : Array<IntArray> = Array(n){ IntArray(m) }
-    var max = -1
-    var min = 257
-    var timeMin=Int.MAX_VALUE
-    var height=0
+   var strArray = bre.readLine().split(" ")
 
-    for(i in 0 until n){
-        var temp = bre.readLine().split(" ").map { it.toInt() }
+    var answer = Integer.MAX_VALUE
 
-        for(j in temp.indices){
-            block[i][j] = temp.get(j)
-            min = Math.min(min,block[i][j])
-            max = Math.max(max,block[i][j])
-        }
-    }
-
-for(i in min until max+1){
-    var inventory = b
-    var time = 0
-
-    for(j in 0 until n){
-
-        for(a in 0 until  m){
-
-            var diff = i-block[j][a]
-
-            if(diff>0){
-                time +=diff
-                inventory -= diff
-
-            }else if(diff<0){
-                time += abs(diff)*2
-                inventory += abs(diff)
+    for(i in 0.. (strArray[1].length - strArray[0].length)){
+        var count =0
+        for(j in strArray[0].indices){
+            if(strArray[1].toCharArray()[j+i]!=strArray[0].toCharArray()[j]){
+                count++
             }
+
         }
 
+        answer = Math.min(count,answer)
     }
-    if(inventory>=0){
-        if(time<=timeMin ){
 
-            timeMin = time
-            height = i
-        }
-    }
-}
-println("${timeMin} ${height}")
+    println(answer)
 }
 
 
