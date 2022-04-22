447ead3d42a80286e241df961a851a63422417c5
jaeil <59818827+jaeilssss@users.noreply.github.com>
2022-04-22 20:14:40 +0900

    로프

--- study/kotlinCodingTest/codingTest36/src/main/kotlin/main.kt
+++ study/kotlinCodingTest/codingTest36/src/main/kotlin/main.kt
@@ -1,36 +1,36 @@
 import kotlin.math.abs
 val bre = System.`in`.bufferedReader()
-fun main() = with(System.out.bufferedWriter()){
 
-    var n = bre.readLine().toInt()
-    var answer = ArrayList<String>()
-    for(i in 0 until  n){
-        var count = bre.readLine().toInt()
-        var str = bre.readLine().split(" ")
-        var temp =""
-        for(j in 0 until count){
-            if(j==0){
-                temp = str[j]
-            }else{
-                if(temp.toCharArray()[0].toString()>=str[j]){
-                    temp = "${str[j]}${temp}"
-                }else{
-                    temp = "${temp}${str[j]}"
-                }
-            }
+fun main() = with(System.out.bufferedWriter()) {
 
+    var count = bre.readLine().toInt()
+
+
+    var list = ArrayList<Int>()
+    var sum = 0
+    var max = 0
+    for(i in 0 until count){
+        var num = bre.readLine().toInt()
+        sum += num
+        list.add(num)
+    }
+
+    list.sortDescending()
+    max  = list[0]
+    sum = list[0]
+    for(i in 1 until list.size){
+
+        var temp   = sum+list[i]
+
+        if(temp.toDouble()/(i+1) >list[i].toDouble()){
+            temp = list[i]*(i+1)
         }
 
-        answer.add(temp)
-    }
+        if(temp>max){
+            max = temp
+            sum = temp
+        }
 
-    for(i in answer.indices){
-        println(answer[i])
     }
+    println(max)
 }
-
-
-
-
-
-
