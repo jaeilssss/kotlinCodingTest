69bc6b136e10e18e1b3d5f8eed5588cf80979cb2
jaeil <59818827+jaeilssss@users.noreply.github.com>
2022-04-23 18:32:49 +0900

    문서 검색

--- study/kotlinCodingTest/codingTest36/src/main/kotlin/main.kt
+++ study/kotlinCodingTest/codingTest36/src/main/kotlin/main.kt
@@ -1,33 +1,24 @@
 
-import kotlin.math.abs
 import java.util.*
-import kotlin.collections.ArrayList
-
 fun main()  {
 
 
     var scanner = Scanner(System.`in`)
 
-
-    var n =scanner.nextInt()
-    var k = scanner.nextInt()
+    var firstStr = scanner.nextLine()
+    var secondStr = scanner.nextLine()
+    var index= 0
     var answer =0
-    var arr = ArrayList<Int>()
-    for(i in 0 until n){
-       arr.add(scanner.nextInt())
-    }
-
-    arr.sortDescending()
-    var index = 0
-    while (k!=0){
-        if(k/arr[index]!=0){
-            var num = k/arr[index]
-
-            k -=num*arr[index]
-            answer+=num
+    while (index+secondStr.length<=firstStr.length){
+        if(firstStr.substring(index,index+secondStr.length).equals(secondStr)){
+            answer++
+            index +=secondStr.length
+        }else{
+            index++
         }
-        index++
-    }
-    println(answer)
 
+
+    }
+
+    println(answer)
 }
