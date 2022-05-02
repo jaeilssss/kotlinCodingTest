30b9b93070ef7616ccdcc60bf14469793ff8b1bb
jaeil <59818827+jaeilssss@users.noreply.github.com>
2022-05-02 21:55:26 +0900

    이진 변환 반복하기 , 양궁

--- study/kotlinCodingTest/codingTest36/src/main/kotlin/Solution.kt
+++ study/kotlinCodingTest/codingTest36/src/main/kotlin/Solution.kt
@@ -1,70 +1,37 @@
 class Solution {
-    fun solution(rows: Int, columns: Int, queries: Array<IntArray>): IntArray {
-        var answer = intArrayOf()
-        answer = IntArray(queries.size)
-        var arr: Array<IntArray> = Array(rows) { IntArray(columns) }
+    fun solution(s: String): IntArray {
+        var answer: IntArray = intArrayOf()
 
-        var num = 1
-        for (i in 0 until rows) {
-            for (j in 0 until columns) {
-                arr[i][j] = num
-                num++
-            }
-
-        }
-
-        for (i in queries.indices) {
-            var x1 = queries[i][0]
-            var y1 = queries[i][1]
-            var x2 = queries[i][2]
-            var y2 = queries[i][3]
-
-            var x = x1 - 1
-            var y = y1
-            var temp = arr[x1 - 1][ y1 - 1]
-            var min = temp
-            while (x != x1 - 1 || y != y1 - 1) {
-
-                min = Math.min(min, arr[x][y])
-                var temp2 = arr[x][y]
-                arr[x][y] = temp
-                temp = temp2
-
-                if (y < y2 - 1 && x == x1 - 1) {
-                    y++
-                } else if (y == y2 - 1 && x != x2 - 1) {
-                    x++
-                } else if (x == x2 - 1 && y > y1 - 1) {
-                    y--
-                } else if (y == y1 - 1 && x > x1 - 1) {
-                    x--
+        answer  = IntArray(2)
+        var count = 0
+        var zeroCount =0
+        var arr = s.toCharArray()
+        while (arr.size!=1){
+            count++
+             var temp = ""
+            for(i in arr.indices){
+                if(arr[i]!='0'){
+                    temp +="1"
+                }else{
+                    zeroCount++
                 }
-
             }
-            arr[x1-1][y1-1] = temp
 
-            min = Math.min(min,  arr[x1-1][y1-1])
-            answer[i] = min
+            arr =Integer.toBinaryString((temp.length)).toCharArray()
+
 
         }
-        for(w in 0 until rows){
-            for(j in 0 until columns){
-                print(arr[w][j])
-            }
-            println()
-        }
 
+        answer[0]=count
+        answer[1]=zeroCount
+
+
+        println(answer[0])
+        println(answer[1])
         return answer
     }
-
 }
-fun main() {
-    var sol = Solution()
 
-    var answer =
-        sol.solution(6, 6, arrayOf(intArrayOf(2, 2, 5, 4,), intArrayOf(3, 3, 6, 6,), intArrayOf(5, 1, 6, 3)))
-
-//    for(i in answer.indices){
-//        println(answer[i])
-//    }
+fun main(){
+    Solution().solution("1111111")
 }
