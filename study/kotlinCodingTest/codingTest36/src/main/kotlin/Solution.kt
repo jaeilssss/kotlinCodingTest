d597dfe509a00b0fa1da0ad91ce9b9ff0731c81f
jaeil <59818827+jaeilssss@users.noreply.github.com>
2022-05-03 20:25:52 +0900

    K진수 변환 후 소수 찾기

--- study/kotlinCodingTest/codingTest36/src/main/kotlin/Solution.kt
+++ study/kotlinCodingTest/codingTest36/src/main/kotlin/Solution.kt
@@ -1,37 +1,82 @@
-class Solution {
-    fun solution(s: String): IntArray {
-        var answer: IntArray = intArrayOf()
+import java.lang.StringBuilder
 
-        answer  = IntArray(2)
-        var count = 0
-        var zeroCount =0
-        var arr = s.toCharArray()
-        while (arr.size!=1){
-            count++
-             var temp = ""
-            for(i in arr.indices){
-                if(arr[i]!='0'){
-                    temp +="1"
-                }else{
-                    zeroCount++
+class Solution {
+    fun solution(n: Int, k: Int): Int {
+        var answer: Int = 0
+
+
+        var str = converter(k , n)
+        var startIndex = -1
+        for(i in str.indices){
+
+            if(startIndex ==-1 && ! str.substring(i,i+1).equals("0")){
+                startIndex = i
+
+                if(i==str.length-1 && !str.substring(i,i+1).equals("0")){
+                    if(startIndex==-1){
+                        startIndex = i
+                    }
+                    var num = str.substring(startIndex,i+1)
+                    if(isPrime(num.toLong())){
+                        answer++
+                    }
                 }
+            } else if(startIndex != -1 && str.substring(i,i+1).equals("0")){
+
+
+                var num = str.substring(startIndex,i)
+                if(isPrime(num.toLong())){
+                    answer++
+                }
+                startIndex = -1
+            }else if(i==str.length-1 && !str.substring(i,i+1).equals("0")){
+                if(startIndex==-1){
+                    startIndex = i
+                }
+                var num = str.substring(startIndex,i+1)
+                if(isPrime(num.toLong())){
+                    answer++
+                }
+
+
+            }
+        }
+        return answer
+    }
+
+    fun converter(digits : Int , num : Int) : String {
+        var number = num
+
+        val  sb = StringBuilder()
+
+        if(number==0) return "0"
+
+        while (number!=0){
+            val remain = number % digits
+
+            if(remain >9){
+                sb.insert(0,(remain+55).toChar())
+            }else{
+                sb.insert(0,remain)
             }
 
-            arr =Integer.toBinaryString((temp.length)).toCharArray()
-
-
+            number /= digits
         }
 
-        answer[0]=count
-        answer[1]=zeroCount
+        return sb.toString()
+    }
+
+    fun isPrime(num : Long) : Boolean{
 
 
-        println(answer[0])
-        println(answer[1])
-        return answer
+        if(num<=1) return false
+        for(i in 2 ..Math.sqrt(num.toDouble()).toInt()){
+            if((num%i).toInt()==0) return false
+        }
+        return true
     }
 }
 
 fun main(){
-    Solution().solution("1111111")
+    println(Solution().solution(524287,2))
 }
