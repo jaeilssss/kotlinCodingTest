05db20ac3d8d2052eccaee4cf51908df6cc5ae7b
jaeil <59818827+jaeilssss@users.noreply.github.com>
2022-05-22 21:11:06 +0900

    화학식량

--- study/kotlinCodingTest/codingTest36/src/main/kotlin/main.kt
+++ study/kotlinCodingTest/codingTest36/src/main/kotlin/main.kt
@@ -2,33 +2,53 @@
 import com.sun.xml.internal.fastinfoset.util.StringArray
 import java.io.BufferedReader
 import java.io.InputStreamReader
+import java.util.*
 
 fun main() {
     val bufferedReader = BufferedReader(InputStreamReader(java.lang.System.`in`))
 
-    var num = bufferedReader.readLine().toInt()
-
     var chars = bufferedReader.readLine().toCharArray()
 
-    var bCount = 0
-    var rCount = 0
-    for(i in 0 until num){
+    var stack = Stack<Int>()
+
+    var answer = 0
+    var check = false
+    var lastNum  =0
+    stack.push(0)
+    for(i in chars.indices){
 
         var c = chars[i]
-        if(i==0){
-            if(chars[i]=='B') bCount++
-            else rCount++
-        }else{
 
-            if(c==chars[i-1]) continue
-            else{
-                if(c=='B') bCount++
-                else rCount++
+            if(c=='H'){
+            var num = stack.pop()
+                stack.push(num+1)
+                lastNum = 1
+            }else if(c=='C'){
+                var num = stack.pop()
+                stack.push(num+12)
+                lastNum=12
+            }else if(c=='O'){
+                var num = stack.pop()
+                stack.push(num+16)
+                lastNum = 16
+            }else if(c=='('){
+                stack.push(0)
+            }else if(c==')'){
+                var num1 = stack.pop()
+                lastNum = num1
+                var num2 = stack.pop()
+                stack.push(num1+num2)
+            }else{
+                var num = chars[i].toString().toInt()
+                for(i in 1 until num){
+                    var temp = stack.pop()
+                    stack.push(temp+lastNum)
+                }
             }
-        }
+
     }
 
-    if(bCount>=rCount) println(rCount+1)
-    else println(bCount+1)
+    println(stack.pop())
+
 }
 
