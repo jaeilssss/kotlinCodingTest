45c5739c79307f3e72f662eff6a33a6b9bb0039f
jaeil <59818827+jaeilssss@users.noreply.github.com>
2022-06-10 16:19:07 +0900

    모두의 마블

--- study/kotlinCodingTest/codingTest36/src/main/kotlin/Solution.kt
+++ study/kotlinCodingTest/codingTest36/src/main/kotlin/Solution.kt
@@ -1,60 +1,34 @@
-import java.util.regex.Pattern
+import java.util.*
+import kotlin.collections.HashMap
 
 class Solution {
-    var answer = HashSet<HashSet<String>>()
-    var list = ArrayList<ArrayList<String>>()
-    fun solution(user_id: Array<String>, banned_id: Array<String>): Int {
+    fun solution(jobs: Array<IntArray>): Int {
+        var answer = 0
+
+        var heap = PriorityQueue<HashMap<Int,Int>>()
 
 
-
-        banned_id.forEach {
-        list.add(getMatched(user_id,it))
-        }
-
-
-        dfs(HashSet(),0)
-
-
-        return answer.size
+        return answer
     }
 
-    fun getMatched(user_id: Array<String>, bannedId : String ) : ArrayList<String>{
-
-        var arrayList = ArrayList<String>()
-        var ban = bannedId.replace('*','.')
-        user_id.forEach {
-
-            if(Pattern.matches(ban,it)){
-                arrayList.add(it)
-            }
-        }
-        return arrayList
-    }
-
-    fun dfs(set : HashSet<String> , depth : Int){
-
-        if(depth==list.size){
-            answer.add(HashSet(set))
-            return
-        }
-        list.get(depth).forEach{
-            if(!set.contains(it)){
-                set.add(it)
-                dfs(set,depth+1)
-                set.remove(it)
-            }
-        }
-
+    fun dfs(){
 
     }
 }
 
-
 fun main(){
-    var solution = Solution()
-    println(solution.solution(
-        arrayOf("frodo", "fradi", "crodo", "abc123", "frodoc"),
-        arrayOf("*rodo", "*rodo", "******")
-    ))
+    var heap = PriorityQueue<HashMap<Int,Int>>()
 
+    var map = HashMap<Int,Int>()
+    map.put(100,10)
+
+    heap.add(map)
+    map.remove(100)
+    map.put(1,1)
+    heap.add(map)
+
+    heap.forEach {
+        var keys = it.keys.toList()
+        println(keys[0])
+    }
 }
--- study/kotlinCodingTest/codingTest36/src/main/kotlin/main.kt
+++ study/kotlinCodingTest/codingTest36/src/main/kotlin/main.kt
@@ -2,44 +2,22 @@
 import com.sun.xml.internal.fastinfoset.util.StringArray
 import java.io.BufferedReader
 import java.io.InputStreamReader
-import java.util.*
-import kotlin.collections.ArrayList
 
 fun main() {
     val bufferedReader = BufferedReader(InputStreamReader(java.lang.System.`in`))
 
-   var num = bufferedReader.readLine().split(" ").map { it.toInt() }
-    var arr = Array<CharArray>(num[0]){ CharArray(num[1]) }
-    var list = ArrayList<String>()
-    for(i in 0 until num[0]){
-        var temp = bufferedReader.readLine().toCharArray()
+    var num = bufferedReader.readLine().toInt()
+    var numList = bufferedReader.readLine().split(" ").map { it.toInt() }
 
-        for(j in 0 until num[1]){
-            arr[i][j] = temp[j]
-        }
+
+  var list =  numList.sortedDescending()
+    var answer = 0
+    var max = list[0]
+    for(i in 1 until list.size){
+        answer += max+list[i]
+
     }
-
-    for(i in 0 until num[0]){
-        var str =""
-        for(j in 0 until num[1]){
-            if(arr[i][j]=='#') break
-           else str +=arr[i][j].toString()
-        }
-        if(str!="" && str.length>=2) list.add(str)
-    }
-
-    for(i in 0 until num[1]){
-        var str = ""
-        for(j in 0 until num[0]){
-          if(arr[j][i]=='#') break
-           else str+= arr[j][i].toString()
-        }
-        if(str!="" && str.length>=2) list.add(str)
-    }
-
-    list.sort()
-
-    println(list[0])
+    println(answer)
 
 }
 
