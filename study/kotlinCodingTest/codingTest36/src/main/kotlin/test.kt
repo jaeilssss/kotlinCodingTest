842557902d438d036807ca67c1f9fbd503e9d99f
jaeil <59818827+jaeilssss@users.noreply.github.com>
2022-11-20 18:44:50 +0900

    병든 나이트

--- study/kotlinCodingTest/codingTest36/src/main/kotlin/test.kt
+++ study/kotlinCodingTest/codingTest36/src/main/kotlin/test.kt
@@ -2,87 +2,57 @@
 import java.util.*
 import kotlin.collections.ArrayList
 
-var arr : Array<CharArray> = arrayOf()
-var visited : Array<BooleanArray> = arrayOf()
-var answer = Int.MIN_VALUE
-var n : Int =0
-var m =0
-var dir = arrayOf(arrayOf(1,0), arrayOf(0,1), arrayOf(-1,0), arrayOf(0,-1))
 fun main() = with(System.out.bufferedWriter()) {
 
-    var list= ArrayList<Point>()
 
-    var scanner = Scanner(System.`in`)
-     n = scanner.nextInt()
-     m = scanner.nextInt()
-    arr = Array(n+1){ CharArray(m+1) }
-    visited = Array(n+1){ BooleanArray(m+1) }
 
-    for(i in 0 until n){
-        var carr = scanner.next().toCharArray()
-        for(j in 0 until m){
+    var (N,M) = readLine()?.split(" ")!!.map { it.toInt() }
 
-            arr[i+1][j+1] = carr[j]
 
-            if(arr[i+1][j+1]=='L'){
-                list.add(Point(i+1,j+1,0))
-            }
+    if(N==1){
+        println(1)
+    }else if(N==2){
+        println(kotlin.math.min(4,(M+1)/2))
+    }else if(M<7){
+        println(kotlin.math.min(4,M))
+    }else{
+        println(M-2)
+    }
+
+
+}
+
+
+
+class Point(var x : Int , var y : Int) :Comparable<Point>{
+
+    override fun compareTo(other:Point) : Int{
+        var thisFirst = 0
+        var thisSecond =0
+        var otherFirst=0
+        var otherSecond=0
+        if(this.x>this.y){
+            thisFirst = this.x
+            thisSecond = this.y
+        }else{
+            thisFirst=this.y
+            thisSecond=this.x
         }
+
+        if(other.x>other.y){
+            otherFirst = other.x
+            otherSecond = other.y
+        }else{
+            otherFirst=other.y
+            otherSecond=other.x
+        }
+
+        if(thisFirst==otherFirst){
+            return otherSecond -thisSecond
+        }
+        return otherFirst-thisFirst
     }
-
-    for(i in list.indices){
-        visited = Array(n+1){ BooleanArray(m+1) }
-
-        var point = list[i]
-
-        search(point.x,point.y)
-    }
-
-    println(answer)
 }
-
-fun search(x : Int, y : Int){
-
-
-
-    var queue = LinkedList<Point>()
-
-    queue.add(Point(x,y,0))
-    visited[x][y] = true
-    var count =0
-    while (!queue.isEmpty()){
-        count++
-        var point = queue.poll()
-        var check = false
-
-
-            for(i in dir.indices){
-                var tempX  =point.x+dir[i][0]
-                var tempY = point.y+dir[i][1]
-
-                if(tempX in 1 until n+1 &&
-                    tempY in 1 until m+1){
-
-                    if(!visited[tempX][tempY] && arr[tempX][tempY]=='L'){
-                        visited[tempX][tempY] = true
-                        queue.add(Point(tempX,tempY,point.dist+1))
-
-                        check = true
-                    }
-                }
-            }
-
-        if(!check)                         answer = Math.max(answer,point.dist)
-
-
-    }
-
-
-}
-
-
-
-class Point(var x : Int , var y : Int , var dist : Int)
 
 
 
