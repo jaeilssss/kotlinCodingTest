79145d0c1dfe935808c0c25a5211c857bac6e6e5
jaeil <59818827+jaeilssss@users.noreply.github.com>
2022-12-10 23:35:03 +0900

    나무꾼 이다솜

--- study/kotlinCodingTest/codingTest36/src/main/kotlin/test.kt
+++ study/kotlinCodingTest/codingTest36/src/main/kotlin/test.kt
@@ -1,82 +1,49 @@
 import sun.management.counter.LongCounter
 import java.lang.StringBuilder
 import java.util.*
-import kotlin.collections.HashSet
-
+import kotlin.collections.ArrayList
 fun main(){
 
     var scanner = Scanner(System.`in`)
 
-    var num = scanner.nextInt()
 
-    var set = HashSet<String>()
-    var cnt = 0
-    var answer = StringBuffer()
-    var mainShape = StringBuffer()
-    for(i in 0 until num){
-         mainShape.append(scanner.next())
+    var n = scanner.nextInt()
+
+    var c = scanner.nextInt()
+
+    var w = scanner.nextInt()
+
+    var answer : Long= Long.MIN_VALUE
+    var list = ArrayList<Long>()
+    var max : Long= Long.MIN_VALUE
+    for(i in 0 until n){
+        var n  = scanner.nextLong()
+
+        max = Math.max(max,n)
+        list.add(n)
     }
+    var zero : Long  = 0
 
+    for(i in 1 .. max){
+        var sum  : Long = 0
+        for(j in list.indices){
+            var cut  : Long= 0
 
-    var forward = StringBuffer()
-    var reverse = StringBuffer()
-    var marr = mainShape.toString().toCharArray()
-    for(i in mainShape.indices){
-        forward.append(mainShape[i])
-        reverse.insert(0, reverse(mainShape[i]))
-    }
-    for(i in 0 until num){
-
-        set.add(forward.toString())
-        set.add(reverse.toString())
-
-        forward.append(forward.toString().toCharArray()[0])
-        reverse.append(reverse.toString().toCharArray()[0])
-
-
-        forward.deleteCharAt(0)
-        reverse.deleteCharAt(0)
-
-    }
-    var count = scanner.nextInt()
-
-    for(i in 0 until count){
-
-        var str = StringBuffer()
-
-        for(i in 0 until num){
-
-            str.append(scanner.next())
-        }
-
-
-        if(set.contains(str.toString())){
-            cnt++
-            for(j in str.indices){
-
-                answer.append(str[j])
-                answer.append(" ")
+            if(list[j]>=i.toLong()){
+                if(list[j].toInt() % (i) ==zero){
+                    cut = (list[j]/i) - 1
+                }else{
+                    cut = list[j]/i
+                }
+                if((w*i*(list[j]/i)-cut*c)>0){
+                    sum += (w*i*(list[j]/i)-cut*c)
+                }
             }
-            answer.append("\n")
         }
+
+        answer = Math.max(answer , sum)
     }
 
-    println(cnt)
-    println(answer.toString())
-
-}
-
-fun reverse(c : Char) : Char{
-
-
-    if(c=='1'){
-        return '3'
-    }else if(c== '2'){
-        return '4'
-    }else if(c== '3'){
-        return '1'
-    }else{
-        return '2'
-    }
+    println(answer)
 }
 
