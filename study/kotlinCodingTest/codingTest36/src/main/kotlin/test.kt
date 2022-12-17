d707a82ecbc3c75fd95b7e47e0755186055a1331
jaeil <59818827+jaeilssss@users.noreply.github.com>
2022-12-17 22:07:11 +0900

    달력

--- study/kotlinCodingTest/codingTest36/src/main/kotlin/test.kt
+++ study/kotlinCodingTest/codingTest36/src/main/kotlin/test.kt
@@ -1,5 +1,3 @@
-import sun.management.counter.LongCounter
-import java.lang.StringBuilder
 import java.util.*
 import kotlin.collections.ArrayList
 import kotlin.math.max
@@ -8,39 +6,59 @@ fun main(){
 
     var scanner = Scanner(System.`in`)
 
+    var arr = IntArray(375){0}
 
-    var  N = scanner.nextInt()
+    var N  = scanner.nextInt()
 
-    var max = 0
+    var list = ArrayList<Point>()
 
-    var list  = ArrayList<String>()
+    var start = -1
+    var height = 0
+    var answer = 0
     for(i in 0 until N){
-        list.add(scanner.next())
+
+        list.add(Point(scanner.nextInt() , scanner.nextInt()))
     }
 
-    list.sortBy { it.length }
+    list.sort()
 
     for(i in list.indices){
 
-        var check  = true
-        for(j in i+1 until list.size){
+        var point = list.get(i)
 
-            var s = list[i]
-            var t = list[j]
+        for(j in point.start ..point.end){
 
-            if(t.substring(0,s.length)==s){
-                check = false
-                break
-            }
+            arr[j] = arr[j]+1
         }
-
-        if(check){
-            max++
-        }
-
     }
 
+    for(i in 1 .. 367){
 
-    println(max)
+        if(arr[i]!=0 && start == -1){
+            start = i
+            height = arr[i]
+        }else if(arr[i]==0 && start!=-1){
+
+            answer += (i - start)*height
+
+            start = -1
+
+            height = 0
+        }
+
+        if(height<arr[i]){
+            height = arr[i]
+        }
+    }
+
+    println(answer)
+
 }
 
+class Point(var start : Int , var end : Int): Comparable<Point>{
+
+    override fun compareTo(other: Point) : Int{
+
+        return (other.end- other.start) - (this.end-  this.start)
+    }
+}
